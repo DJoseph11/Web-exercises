c4c5bd65e4affac334f8ee6b4587c7ece5e23972
Darlhem J <joseph.darlhem@gmail.com>
2020-11-04 12:37:38 -0600

    atempting to create a typeof responce from prompt

---
+++ JavaScript_lesson.js
@@ -0,0 +1,12 @@
+// I was trying to make a small program to take an input ie. number
+//strings for the program to output the typeof type of the input
+var checkingTypeOf = function(item) {
+    var text = prompt("Enter Option:");
+    return function(text) {
+        console.log(typeOf item)
+    }
+    return
+
+}
+
+checkingTypeOf();

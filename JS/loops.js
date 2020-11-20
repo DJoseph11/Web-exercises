193fb28bddf4f9184e49227602e175bd866d2b79
Darlhem J <joseph.darlhem@gmail.com>
2020-11-20 09:40:26 -0600

    Added a css external stylesheet

--- JS/loops.js
+++ JS/loops.js
@@ -1,15 +1,15 @@
-var i = 0;
-
-while (i < 10 ){
-	console.log("while loop it " + i);
-	i++
-}
-
-
-// Do while loop
-i = 10
-
-do {
-	console.log("while loops it # " + i );
-	i++
-} while (i < 10);
+// var i = 0;
+//
+// while (i < 10 ){
+// 	console.log("while loop it " + i);
+// 	i++
+// }
+//
+// fjkfjdkjkjfdkllkkkdfdi
+// // Do while loop
+// i = 10
+//
+// do {
+// 	cons  ole.log("while loops it # " + i );
+// 	ii++
+// } while (i < 10adfkjfkd

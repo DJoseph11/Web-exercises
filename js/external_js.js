35aafae695cca26a799b137a35a8c338448d4b6c
Darlhem J <joseph.darlhem@gmail.com>
2020-12-03 13:30:56 -0600

    Cleaning up codes with comments

---
+++ js/external_js.js
@@ -0,0 +1,70 @@
+// console.log("Hello from external javaScript")
+//
+// alert('Welcome to my Website')
+//
+// var userInput = prompt("what is your favorite color?");
+//
+// alert("Great " + userInput + " is my favorite color too!")
+
+// rented movies
+
+// Little mermaid 3 days
+// var mov1 = 3;
+var mov1 = prompt("How many days would you like to rent the 'The Little Mermaid?")
+// Brother bear 5 days
+// var mov2 = 5;
+var mov2 = prompt("How about 'Brother Bear' many days would you like to rent that one?")
+// Hercules 1 day
+// var mov3 = 1;
+var mov3 = prompt("'Hercules' great movie how many days would you like to rent this?")
+// price rented per day is $3
+var price = 3;
+// input is how many days
+var add = (Number(mov1) +Number(mov2) + Number(mov3));
+// add how many days times 3
+var total = add * price;
+// output is the total amount for all three movies
+// console.log(total);
+alert("for a total of " + add + " days, your total comes out to be $" + total + ".")
+
+
+
+//
+// // Work
+//
+// // 3 companies
+// // input is the hours and amount
+// // Google $400 // hours 6
+// var google = 400 * 6;
+// // total for Google
+//
+// // Amazon $380 // hours 4
+// var amazon = 380 * 4;
+// // total for Amazon
+//
+// // Facebook $350 // hours 10
+// var faceBook = 350 * 10;
+// // total for Facebook
+// var total_2 = google + amazon + faceBook;
+// // out total for three companies
+// console.log(total_2);
+//
+// // Student
+//
+// // student can enrolled if class !== full
+// var notfull = true;
+// // class schedule does not conflict with current
+// var schedule = true;
+// var current = true;
+// // option result for the student
+// console.log(notfull = schedule !== current);
+//
+// // offer
+//
+// // product offer conditions
+// //product > 2 offer not expired
+// var promo_1 = product > 2;
+// //product >= 0 offer not expired
+// var promo_2 = product >= 0;
+
+

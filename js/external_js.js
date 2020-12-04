5b2409e1e4e06682e49877c39572e6e41150f220
Darlhem J <joseph.darlhem@gmail.com>
2020-12-03 19:00:40 -0600

    Refactored code

--- js/external_js.js
+++ js/external_js.js
@@ -34,31 +34,49 @@ alert("for a total of " + add + " days, your total comes out to be $" + total + 
 //
 // // 3 companies
 // // input is the hours and amount
+var workHours = prompt("How many hours did you work for google?")
 // // Google $400 // hours 6
-// var google = 400 * 6;
-// // total for Google
-//
+var googAmount = prompt("How about the hourly rate?")
+// var google = 400 * 6; // // total for Google
+var  googTotal = Number(googAmount) * Number(workHours)
+
 // // Amazon $380 // hours 4
+var workHours_2 = prompt("How many hours did you work for Amazon?")
 // var amazon = 380 * 4;
+var amAmount = prompt("How about the hourly rate?")
 // // total for Amazon
-//
+var  amTotal = Number(amAmount) * Number(workHours_2)
+
 // // Facebook $350 // hours 10
+var workHours_3 = prompt("How many hours did you work for Facebook?")
 // var faceBook = 350 * 10;
+var faAmount = prompt("How about the hourly rate?")
 // // total for Facebook
-// var total_2 = google + amazon + faceBook;
-// // out total for three companies
-// console.log(total_2);
+var  faTotal = Number(faAmount) * Number(workHours_3)
+var total_2 = googTotal + amTotal + faTotal;
+alert("Well here's the breakdown, hours at Google: " +
+	workHours + " hours at Amazon: " + workHours_2 + " Facebook: " + workHours_3 + ".")
+alert("And the total for hourly pay: 1. $" + googAmount + " 2. $" + amAmount + " 3. $" + faAmount + "." )
+alert("Your total payment is : " + total_2 + ".")
+// out total for three companies
+console.log(total_2);
 //
 // // Student
-//
+
 // // student can enrolled if class !== full
-// var notfull = true;
+var notfull = true;
+var stuNotFull = confirm("The class, is it full?");
 // // class schedule does not conflict with current
-// var schedule = true;
-// var current = true;
+var stuConflict = confirm("Does the schedule effects your current one? ")
+var schedule = true;
+var current = true;
+
 // // option result for the student
 // console.log(notfull = schedule !== current);
-//
+var notfull = true;
+var schedule = true;
+var current = true;
+
 // // offer
 //
 // // product offer conditions

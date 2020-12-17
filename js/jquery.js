322e22b87fc2f0451fe1b5bf6287b3a674f43be0
Darlhem J <joseph.darlhem@gmail.com>
2020-12-17 16:25:18 -0600

    Added the alert for the h1 content

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,31 @@
 
+
 $(document).ready(function() {
-	alert( 'The DOM has finished loading!' );
-});
+	alert("the system is ready");
+
+	$(".item-container, li").css("font-size", "20px");
+
+	$("h1, p, li").css("background-color", "yellow");
+
+	var content = $("#title").text()
+
+	alert(content)
+})
+
+// var content =  $("#item-list").html();
+//
+// alert(content);
+//
+// // Similar to .innerHTML method
+// // $(".class").html
+//
+// // similar to .style method
+// //$("#id");.css
+//
+// //this how we select an element by id
+//
+// // $("#some-id");
+// //
+
+
+

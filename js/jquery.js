bad71f328d9a901277055de73215fb75e8ec369c
Darlhem J <joseph.darlhem@gmail.com>
2020-12-18 13:36:50 -0600

    Added the jquery next lesson and konami code

--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,55 @@
 
 
+// $(document).ready(function() {
+// 	alert("the system is ready");
+//
+// 	$(".item-container, li").css("font-size", "20px");
+//
+// 	$("h1, p, li").css("background-color", "yellow");
+//
+// 	var content = $("#title").text()
+//
+// 	alert(content)
+// })
+
 $(document).ready(function() {
+	
+	$("h1").click(function() {
+		$(this).animate({left: "250px",})
+		// $(this).css()
+		
+
+		 $(this).mouseenter(function() {
+			 $(this).css("background-color", "blue").fadeOut(100)
+			 $(this).fadeIn(100)
+		 }).mouseleave(function() {
+			 $(this).css("background-color", "blue").fadeOut(100)
+			 $(this).fadeIn(100)
+		 })
+
+		       
+
+	}).mouseleave(function() {
+		// $(this).css("background-color", "red")
+
+
+		$(this).mouseenter(function() {
+			$(this).fadeOut(1000)
+			$(this).css("background-color", "red").fadeIn(100)
+		}).mouseleave(function() {
+			$(this).fadeOut(1000)
+			$(this).css("background-color", "blue").fadeIn(100)
+		})
+	})
+
+
 	alert("the system is ready");
 
-	$(".item-container, li").css("font-size", "20px");
+	  // $("h1").css("background-color")
 
-	$("h1, p, li").css("background-color", "yellow");
-
-	var content = $("#title").text()
-
-	alert(content)
 })
+
+
 
 
 // var content =  $("#item-list").html();
---
+++ js/konami_code.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+// 	alert("the system is ready");
+//
+// 	$(".item-container, li").css("font-size", "20px");
+//
+// 	$("h1, p, li").css("background-color", "yellow");
+//
+// 	var content = $("#title").text()
+//
+// 	alert(content)
+// })
+
+
+
+
+
+
+

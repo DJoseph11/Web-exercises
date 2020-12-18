1f367eedee594519e0486d0ad852100ac37fa155
Darlhem J <joseph.darlhem@gmail.com>
2020-12-18 16:59:06 -0600

    Idded some js code to the project

--- js/konami_code.js
+++ js/konami_code.js
@@ -9,7 +9,30 @@
 //
 // 	alert(content)
 // })
+$(document).keyup(function(e) {
 
+	var code = [38, 40, 37, 39, 66, 65, 13];
+	var codeKey = e.keyCode
+	var count = 0;
+	for(var i = 0; i < code.length; i++) {
+		if(code[i] === codeKey) {
+
+			console.log(code[i] + " You have added 30 lives!")
+		}
+	}
+
+
+	if( codeKey === "") {
+		count = count + 1
+		console.log("wrong key")
+	}
+
+	if(count > 3) {
+		alert("refresh page")
+	}
+//          console.log("hey")
+//    }
+})
 
 
 

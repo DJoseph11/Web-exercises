1d4b86426c826ed6cc3e4b788e1d5294f5e5b3f3
Darlhem J <joseph.darlhem@gmail.com>
2020-12-26 15:37:33 -0600

    starting fresh for the konami code

--- js/konami_code.js
+++ js/konami_code.js
@@ -9,29 +9,34 @@
 //
 // 	alert(content)
 // })
-$(document).keyup(function(e) {
+// $(document).keyup(function(e) {
 
-	var code = [38, 40, 37, 39, 66, 65, 13];
-	var codeKey = e.keyCode
-	var count = 0;
-	for(var i = 0; i < code.length; i++) {
-		if(code[i] === codeKey) {
+// 	var code = [38, 40, 37, 39, 66, 65, 13];
+// 	var codeKey = e.keyCode
+// 	var count = 0;
+// 	for(var i = 0; i < code.length; i++) {
+// 		if(code[i] === codeKey) {
 
-			console.log(code[i] + " You have added 30 lives!")
-		}
-	}
+// 			console.log(code[i] + " You have added 30 lives!")
+// 		}
+// 	}
 
 
-	if( codeKey === "") {
-		count = count + 1
-		console.log("wrong key")
-	}
+// 	if( codeKey === "") {
+// 		count = count + 1
+// 		console.log("wrong key")
+// 	}
 
-	if(count > 3) {
-		alert("refresh page")
-	}
-//          console.log("hey")
-//    }
+// 	if(count > 3) {
+// 		alert("refresh page")
+// 	}
+// //          console.log("hey")
+// //    }
+// })
+
+
+$(function() {
+	alert("Am working");
 })
 
 

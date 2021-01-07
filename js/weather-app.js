8141e553520d77ff168233b97217fb8c7b52f14b
Darlhem J <joseph.darlhem@gmail.com>
2021-01-07 13:47:28 -0600

    added some fot the cards properties

--- js/weather-app.js
+++ js/weather-app.js
@@ -2,20 +2,22 @@
 
 $(function() {
 	console.log("am working")
+	$("li").addClass("")
 	//AJAX call to OpenWeather API
 	$.get("http://api.openweathermap.org/data/2.5/weather", {
 		appid: openWeatherToken,
 		q:     "San Antonio, US",
 		units: "imperial"
 	}).done(function(data) {
-		console.log(data.name);
-		console.log("temp: " + data.main.temp);
+		console.log(`name: ${data.name}`)
+		console.log(`temp: ${data.main.temp}`);
 		console.log(`temp max: ${data.main.temp_max}`);
 		console.log(`temp min: ${data.main.temp_min}`);
 		console.log(`humidity: ${data.main.humidity}`);
-		console.log(data.main.speed);
-		console.log(data.main.sunrise);
-		console.log(data.main.sunset);
+		console.log(`wind: ${data.wind.speed}`);
+		console.log(`sunrise: ${data.sys.sunrise}`);
+		console.log(`sunset: ${data.sys.sunset}`);
+		console.log(data.keys);
 		console.log(data);
 
 	});

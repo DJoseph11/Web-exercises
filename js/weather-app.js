934629fbb3d6c73df17a49950d4c20f71a226de2
Darlhem J <joseph.darlhem@gmail.com>
2021-01-09 10:48:02 -0600

    add the seperate parts for the time app

--- js/weather-app.js
+++ js/weather-app.js
@@ -2,84 +2,74 @@
 
 $(function() {
 	console.log("am working")
-	let http = "https://api.openweathermap.org/data/2.5/weather";
-	let wUnits = "Imperial"
 	
-
 
 // ----------------------Lab #1-------------
 // Goal: to capture the input of the search field
 
 // Time Block
 
-let [hour, minute, second] = new Date().toLocaleTimeString("en-US").split(/:| /);
-
-console.log(hour + ":" + minute);
-
 let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/");
 
-
 // Shortcuts for jQuery
-let info = {
+let getWeather, http, info = {
+	cityName:$("#cityNameDisplay"),
+	humidity: $("#humidity"),
+	location: $("#cityNameSearch"),
 	temperature: $("#temperature"),
 	tempMax: $("#weather-min-temperature"),
 	tempMin: $("#weather-max-temperature"),
-	humidity: $("#humidity"),
-	wind: $("#weather-wind-speed"),
-	location: $("#cityNameSearch"),
 	sunrise: $(".weather-sunrise"),
 	sunset: $(".weather-sunset"),
-	searchButton: $("#cityNameSearchButton")
+	searchButton: $("#cityNameSearchButton"),
+	wind: $("#weather-wind-speed")
 }
 
-
-// this set the data on the html page layout
-// let displayData = function(data) {
-// 	info.location.text(`${data.name}, ${data.sys.country}`);
-// 	info.temperature.text(`${data.main.temp}`);
-// 	info.tempMax.text(`${data.main.temp_max}`);
-// 	info.tempMin.text(`${data.main.temp_min}`);
-// 	info.humidity.text(`${data.main.humidity}`);
-// 	info.wind.text(`${data.wind.speed}`);
-// 	info.sunrise.text(`${data.sys.sunrise}`);
-// 	info.sunset.text(`${data.sys.sunset}`);
+	http = "https://api.openweathermap.org/data/2.5/weather";
 	
-// }
-
+	getWeather = $.get(`${http}`, {
+		APPID: openWeatherToken,
+		q: "Miami ,US",
+		units: "imperial"
+	
+	})
 
 // keyboard Event to capture the enter key
 
-		info.location.keypress(function(e){
-			// console.log(e)
-			if(e.keyCode == 13) {
-				info.searchButton.click();
-			}
-		})
+info.location.keypress(function(e){
+	// console.log(e)
+	if(e.keyCode == 13) {
+		info.searchButton.click();
+	}
+})
 
-		info.searchButton.click(function(){
-		
-		$.get(`${http}`, {
-			APPID: openWeatherToken,
-			q: info.location.val() + ",US",
-			units: "imperial"
-		
-		}).done(function(data) {
-			info.location.text(`${data.name}`);
-			info.temperature.text(`${data.main.temp}`);
-			info.tempMax.text(`${data.main.temp_max}`);
-			info.tempMin.text(`${data.main.temp_min}`);
-			info.humidity.text(`${data.main.humidity}`);
-			info.wind.text(`${data.wind.speed}`);
-			info.sunrise.text(`${data.sys.sunrise}`);
-			info.sunset.text(`${data.sys.sunset}`);
-			console.log(data);
-		
-		
-		});
+// click event chained to the keypress event
+
+info.searchButton.click(function(){
+
+	getWeather.done(function(data) {
+
+	// this set the data on the html page layout
+
+	info.cityName.text(`${data.name}`);
+	info.temperature.text(`${data.main.temp}`);
+	info.tempMax.text(`${data.main.temp_max}`);
+	info.tempMin.text(`${data.main.temp_min}`);
+	info.humidity.text(`${data.main.humidity}`);
+	info.wind.text(`${data.wind.speed}`);
+	info.sunrise.text(`${data.sys.sunrise}`);
+	info.sunset.text(`${data.sys.sunset}`);
+	console.log(data);
+	console.log(data.name);
 
 
-	})
+});
 
+
+})
+
+
+//
 
 //----------------------------------------------
 
@@ -88,9 +78,13 @@ let info = {
 
 
 // ----------------------Lab #2-------------
+// goal to convert sunrise and sunset
 
 
 
+let [hour, minute, second] = new Date().toLocaleTimeString("en-US").split(/:| /);
+
+console.log(myFunction() + " "+ months() + ", " + date)
 
 
 

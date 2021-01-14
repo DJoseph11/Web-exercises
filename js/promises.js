4bb8e60e6f56104e6d7f12ab340dfe36d0c5ce0f
Darlhem J <joseph.darlhem@gmail.com>
2021-01-13 21:37:59 -0600

    added the chaining method

--- js/promises.js
+++ js/promises.js
@@ -4,6 +4,19 @@
 
 
     const myPromise = fetch("https://api.github.com/users");
-    myPromise.then(response => console.log(response));
-    myPromise.catch(error => console.log(error));
-    
+    const githubPromise = fetch("https://api.github.com/repositories");
+    const bitbucketPromise = fetch("https://api.bitbucket.org/2.0/repositories");
+    // .then: accepts a callback that will run when the promise is resolved.
+    // myPromise.then(response => console.log(response));
+
+    // // .catch: accepts a callback that will run when the promise is rejected.
+    // myPromise.catch(error => console.log(error));
+
+
+    Promise.all([githubPromise, bitbucketPromise, myPromise])
+    .then(data => {
+console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })

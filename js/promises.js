9a131465381a1e770b58dbe150aa8d5bd418ea9a
Darlhem J <joseph.darlhem@gmail.com>
2021-01-13 19:57:07 -0600

    refactor the code to a const variable

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,9 @@
-tgtg
+
+    
+    // .then(data => console(data))
+
+
+    const myPromise = fetch("https://api.github.com/users");
+    myPromise.then(response => console.log(response));
+    myPromise.catch(error => console.log(error));
+    

e3393d5b3d21d3318944b495c3647f80b316a4ea
Darlhem J <joseph.darlhem@gmail.com>
2021-01-14 09:01:35 -0600

    added the promises

--- js/promises.js
+++ js/promises.js
@@ -13,10 +13,62 @@
     // myPromise.catch(error => console.log(error));
 
 
-    Promise.all([githubPromise, bitbucketPromise, myPromise])
-    .then(data => {
-console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    })
+    // Promise.all([githubPromise, bitbucketPromise, myPromise])
+    // .then(data => {
+    //     console.log(data);
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    // })
+
+    Promise.resolve('jake_one').then((one) => {
+        // .resolve() immediately return a resolved promise with the
+        // value. 
+        console.log(one);
+        return 'two_bird';
+    }).then((tw) => {
+        console.log(tw);
+        return 'three_head';
+    }).then((three) => {
+        console.log(three);
+        return "bald dad"
+    }).then((dad) => {
+        console.log(dad)
+    });
+    
+    // the passed value jake_one would get attach to (one) then then
+    // the callback function would return a value that can also be treated
+    // as a promise. and then you can another promise to that in then
+    // above example (tw) is assigned the return value of jake_one which
+    // was two_bird then so on and so forth
+
+myPromise.then(res => {
+    res.json().then((users) => {
+        users.forEach(user => {
+            // console.table(user.login);
+        });
+    } );
+});
+
+
+function getHubUserName() {
+    return myPromise.then(res => res.json());
+}
+
+// getHubUserName().then(users => {
+//     users.forEach(user => {
+//         console.log(user.login);
+//     });
+// }).catch(error => {
+//     console.error(error)
+// })
+
+const secondPromise = new Promise((resolve, reject) => {
+    if (Math.random() > 0.5) {
+        resolve();
+        } else {
+            reject();
+        }
+})
+
+console.log(SecondPromise)
